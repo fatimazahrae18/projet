d921d4c036b4fa5905f111d377b9ee44b51d1f2e
FATOMAJI <jilalik354@gmail.com>
2025-04-30 18:34:08 +0100

    Add initial project folders

---
+++ simpleQuiz/SimpleQuiz.js
@@ -0,0 +1,13 @@
+const question = [
+    {
+        question:"which is larget animal in the world?",
+        answers:[
+            {text:"Shark",correct:false},
+            {text:"Blue whale",correct:true},
+            {text:"Elphant",correct:false},
+            {text:"Giraffe",correct:false},
+
+
+        ]
+    }
+]
